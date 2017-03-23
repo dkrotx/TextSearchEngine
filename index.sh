2323d664cd0bfed2d12d42b3a671206e8f2217ec
Grigory Turunov <g.turunov@corp.mail.ru>
2017-03-23 17:26:36 +0300

    Auto generate scripts

--- index.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-$( dirname $0 )/tsengine_run.sh -Xmx2048m org.bytesoft.tsengine.demo.HtmlIndexerDemo "$@"
--- make_dict.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-$( dirname $0 )/tsengine_run.sh org.bytesoft.tsengine.demo.DictionaryMaker "$@"
