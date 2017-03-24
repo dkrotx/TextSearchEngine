ab7db4157a76a4e42abb38bd340df58ebbb6b7fa
Grigory Turunov <g.turunov@corp.mail.ru>
2017-03-24 15:22:06 +0300

    Refactor gitignores

--- examples/prepare_docs/convert.sh
+++ examples/prepare_docs/convert.sh
@@ -3,7 +3,7 @@
 set -o pipefail
 
 function Err() {
-  echo $1 > &2
+  echo $1 >&2
   exit 1
 }
 
@@ -13,5 +13,6 @@ OUTPUT_FILE=${2:-documents.binary.gz}
 [ -f ${INPUT_FILE} ] || Err "Input file not found: ${INPUT_FILE}"
 
 rm -f ${OUTPUT_FILE} || Err "Failed to remove output file: ${OUTPUT_FILE}"
+[ -d $(dirname ${OUTPUT_FILE} ) ] || mkdir -p $( dirname ${OUTPUT_FILE} )
 
 ./ppc/convert.py -i ${INPUT_FILE} -p protogen.plain_document_pb2 -m PlainDocument --json | gzip > ${OUTPUT_FILE}
--- examples/prepare_docs/prepare_basic.sh
+++ examples/prepare_docs/prepare_basic.sh
@@ -6,13 +6,17 @@ pushd ${SCRIPT_DIR}
 
 DOCUMENT_LIST=${1:-"documents.list"}
 OUTPUT_GZ=${2:-"../parallel_index/text.parsed/documents.binary.gz"}
+
+echo "Download"
 ./download.sh "https://www.amazon.com/gp/product/B00ZV9PXP2" $DOCUMENT_LIST
 ./download.sh "https://en.wikipedia.org/wiki/United_Nations_Scientific_Committee_on_the_Effects_of_Atomic_Radiation" $DOCUMENT_LIST
 ./download.sh "https://en.wikipedia.org/wiki/Palace_of_Westminster" $DOCUMENT_LIST
 ./download.sh "https://blog.cloudera.com/blog/2016/12/hdfs-datanode-scanners-and-disk-checker-explained/" $DOCUMENT_LIST
 
+echo "Prepare protobuf"
 ./python_proto.sh
 
+echo "Convert docs from json to proto"
 ./convert.sh ${DOCUMENT_LIST} ${OUTPUT_GZ}
 
 popd
